f17bf097fd5573b5c4fffab2c62ee492d8ff2057
martinstervander <martin@stervander.com>
2022-01-21 15:43:35 +0000

    Added Swedish

--- test-code.R
+++ test-code.R
@@ -7,3 +7,7 @@ print("hello")
 x <- 1:10
 
 #Hi Natalie, THank you so much for this course! It's been INCREDIBLE! Have a wonderful weekend :) 
+
+# There's too little Swedish in your code
+
+print("Mer svenska, tack! Du rockar.")
